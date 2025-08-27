dbc5a8a00010ad5f599beb2a8922b933587ecfbe
ShaneK <shane@shanessite.net>
2025-08-27 09:20:53 -0700

    Fixing ngIf issue

--- src/app/version.ts
+++ src/app/version.ts
@@ -2,5 +2,5 @@
  * Application version
  * Update this when deploying new versions to force PWA updates
  */
-export const APP_VERSION = '1.0.1';
+export const APP_VERSION = '1.0.2';
 export const BUILD_DATE = new Date().toISOString();
