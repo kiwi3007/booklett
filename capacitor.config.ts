6147cc0865f61fa23dd3b15a9f54900333f22c02
ShaneK <shane@shanessite.net>
2025-08-26 20:43:12 -0700

    Adding PWA support, favicon

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,7 +1,7 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
+  appId: 'app.booklett.mobile',
   appName: 'Booklett',
   webDir: 'www'
 };
--- src/main.ts
+++ src/main.ts
@@ -2,13 +2,14 @@ import { bootstrapApplication } from '@angular/platform-browser';
 import { RouteReuseStrategy, provideRouter, withPreloading, PreloadAllModules } from '@angular/router';
 import { IonicRouteStrategy, provideIonicAngular } from '@ionic/angular/standalone';
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
-import { APP_INITIALIZER, inject } from '@angular/core';
+import { APP_INITIALIZER, inject, isDevMode } from '@angular/core';
 
 import { routes } from './app/app.routes';
 import { AppComponent } from './app/app.component';
 import { apiInterceptor } from './app/core/interceptors/api.interceptor';
 import { ServerSettingsService } from './app/core/services/server-settings.service';
 import { ThemeService } from './app/core/services/theme.service';
+import { provideServiceWorker } from '@angular/service-worker';
 
 export function initApp() {
   return async () => {
@@ -25,6 +26,9 @@ bootstrapApplication(AppComponent, {
     { provide: APP_INITIALIZER, useFactory: initApp, multi: true },
     provideHttpClient(withInterceptors([apiInterceptor])),
     provideIonicAngular(),
-    provideRouter(routes, withPreloading(PreloadAllModules)),
+    provideRouter(routes, withPreloading(PreloadAllModules)), provideServiceWorker('ngsw-worker.js', {
+            enabled: !isDevMode(),
+            registrationStrategy: 'registerWhenStable:30000'
+          }),
   ],
 });
