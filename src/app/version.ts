a2975bbf613ce02eb4945be897027ac038bd238d
ShaneK <shane@shanessite.net>
2025-08-27 20:05:12 -0700

    Trying to fix some minor issues

--- src/app/version.ts
+++ src/app/version.ts
@@ -2,5 +2,5 @@
  * Application version
  * Update this when deploying new versions to force PWA updates
  */
-export const APP_VERSION = '1.1.1';
+export const APP_VERSION = '1.1.2';
 export const BUILD_DATE = new Date().toISOString();
